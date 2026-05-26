53e36c11664cb8f4b4b3edd7474aec71d6b67510
SK_DATA_ENGINEER <64856267+skaba89@users.noreply.github.com>
2026-05-26 11:59:50 +0200

    Register user model

--- platform/backend/app/models/__init__.py
+++ platform/backend/app/models/__init__.py
@@ -1,3 +1,6 @@
 from app.models.organization import Organization
 from app.models.contact import Contact
 from app.models.opportunity import Opportunity
+from app.models.user import User
+
+__all__ = ["Organization", "Contact", "Opportunity", "User"]
